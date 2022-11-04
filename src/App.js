938ee830fd09533d476c685b91a61144df9a8868
HoanTD <hoantd@vnext.vn>
2022-11-04 13:46:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
+import ColorBox from './components/ColorBox';
+import PostList from './components/PostList';
 
 function App() {
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'https://picsum.photos/v2/list?page=1&limit=10';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        
+        setPostList(responseJSON);
+      } catch (error) {
+        console.log('Failed to fetch post list', error.message);
+      }
+      
+    }
+
+    fetchPostList();
+  },[]);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Post list</h1>
+      <PostList posts={postList}></PostList>
+
     </div>
   );
 }
